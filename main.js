e972012fa139e49efb1aade3456ebe571e260e96
samuelhuang <pedoe1989@gmail.com>
2017-03-24 13:30:25 +0800

    Add render function to decrease code size

--- main.js
+++ main.js
@@ -6,108 +6,18 @@ $(document).ready(function(){
 	var total_page = 1;
 	var items = '<table id="paginate" id="table1" border="'+1+'">';
 
-
-	$('#itemsNumInput').click(function(e){
-		number_of_items = $('#itemsNum').val();
-
-		items = '<table id="paginate" id="table1" border="'+1+'">';
-		
-		for (var i = 1; i <= number_of_items; i++){
-			items += '<tr>' + '<td>' + i + '</td>' + '<td>' + 'Item' + i +'</td>' + '</tr>'; 
-		}
-		items += '</table>';
-		$('#paginate').html(items);
-
-		navigation_html = '<a class="first_link" href=""><<</a>';
-		navigation_html += '<a class="previous_link" href="">Prev</a>';	
-		total_page = 1;
-		for (var i = 0; i < number_of_items; i = i + show_per_page) {
-			navigation_html += '<a class="page_link" href="" data-start="' + i + '" data-end="' + (i + show_per_page) + '">' + (total_page) + '</a>';
-			total_page++;
-		}
-		
-		navigation_html += '<a class="next_link" href="">Next</a>';
-		navigation_html += '<a class="last_link" href="">>></a>';
-		$('#page_navigation').html(navigation_html); //.html() change all the content of elements
-		rowDisplay(0, show_per_page);
-		$('.page_link').first().addClass('active');
-	});
+	render();
 	
-	for (var i = 1; i <= number_of_items; i++){
-		items += '<tr>' + '<td>' + i + '</td>' + '<td>' + 'Item' + i +'</td>' + '</tr>'; 
-	}
-	items += '</table>';
-	$('#paginate').html(items);
-	navigation_html += '<a class="previous_link" href="">Prev</a>';
-
-// Create all the element and asign href link.
-	for (var i = 0; i < number_of_items; i = i + show_per_page) {
-		navigation_html += '<a class="page_link" href="" data-start="' + i + '" data-end="' + (i + show_per_page) + '">' + (total_page) + '</a>';
-		total_page++;
-	}
-
-// After all the navigation_html are created, add the Next link 
-// and $('#page_navigation').html(navigation_html) will create the navigation bar in the html
-// which is located in <div id=page_navigation></div>
-	navigation_html += '<a class="next_link" href="">Next</a>';
-	navigation_html += '<a class="last_link" href="">>></a>';
-	$('#page_navigation').html(navigation_html); //.html() change all the content of elements
-	rowDisplay(0, show_per_page);
-	console.log(navigation_html);
-
-//hide() all the element and select the element you want to show
-//slice() method selects a subset of elements based on its index	
-	function rowDisplay(startIndex, endIndex) {
-		$('#paginate tr').hide().slice(startIndex, endIndex).show();
-	}
-
-	function pagination(c, m){
-		var current = c,
-			last = m,
-			delta = 2,
-			left = current - delta,
-			right = current + delta + 1,
-			range = [],
-			rangeWithDots = [],
-			l;
-
-		for (let i = 1; i <= last; i++){
-			if(i == 1 || i == last || i >= left && i < right){
-				range.push(i);
-			}
-		}
-
-		for (let i of range){
-			if (l){
-				if (i - l == 2){
-					rangeWithDots.push(l + 1);
-				}else if (i - l !== 1){
-					rangeWithDots.push('...');
-				}
-			}
-		}
-
-		rangeWithDots.push(i);
-		l = i;
-
-		return rangeWithDots;
-	}
-
-
-
-
-// Must bind with document to avoid the new '.page_link' can't have click event
+	// Must bind with document to avoid the new '.page_link' can't have click event
 	$(document).on('click', '.page_link', function(e){
 		e.preventDefault(); //Cancel the default action when the method is called
 		$('.active').removeClass('active');
 		$(this).addClass('active');
 		var IndexData = $(this).data();
-		console.log(IndexData);
 		rowDisplay(IndexData.start, IndexData.end);	
 	});
 	
-	$('.page_link').first().addClass('active');  //.first() constructs a new jquery object from the first element in that set, and add the element to active class. This is for page show class with active at first time.
-
+	// Previous, Next function
 	$(document).on('click', '.previous_link, .next_link', function(e){
 		e.preventDefault();
 		var traverse = $(this).is('.previous_link') ? 'prev' : 'next';
@@ -115,12 +25,11 @@ $(document).ready(function(){
 		$('.page_link.active')[traverse]('.page_link').click(); 
 	});
 
-// First, Last Function
+	// First, Last Function
 	$(document).on('click', '.first_link, .last_link', function(e){
 		e.preventDefault();
 		
 		if($(this).is('.first_link')){
-			console.log('show_per_page in the first_link', show_per_page);
 			rowDisplay(0, show_per_page);
 			$('.page_link').removeClass('active').first().addClass('active');
 		}
@@ -134,10 +43,9 @@ $(document).ready(function(){
 			}
 			$('.page_link').removeClass('active').last().addClass('active');
 		}
-		
 	});
 
-// Search Function
+	// Search Function
 	$('#searchSubmit').click(function(e){
 		var page = $('#searchInput').val();
 		var data = page * show_per_page;
@@ -154,23 +62,57 @@ $(document).ready(function(){
 		
 	});
 
+	// Show per page function
 	$('#page_per_show').change(function(e){
 		e.preventDefault();
 		// The value get from the jQuery is not int, so use the parseInt to convert
 		show_per_page = parseInt($('#page_per_show option:selected').val());
-		navigation_html = '<a class="first_link" href=""><<</a>';
-		navigation_html += '<a class="previous_link" href="">Prev</a>';	
+		render();
+	});
+
+	// Input mock data function
+	$('#itemsNumInput').click(function(e){
+		number_of_items = $('#itemsNum').val();
+		render();
+		$('.page_link').first().addClass('active');
+	});
+
+	function render(){
+		
 		total_page = 1;
+
+		//Create table data
+		items = '<table id="paginate" id="table1" border="'+1+'">';
+		for (var i = 1; i <= number_of_items; i++){
+			items += '<tr>' + '<td>' + i + '</td>' + '<td>' + 'Item' + i +'</td>' + '</tr>'; 
+		}
+		items += '</table>';
+		$('#paginate').html(items);
+	
+		// Create all the element and asign href link.
+		navigation_html = '<a class="first_link" href=""><<</a>';
+		navigation_html += '<a class="previous_link" href="">Prev</a>';
 		for (var i = 0; i < number_of_items; i = i + show_per_page) {
 			navigation_html += '<a class="page_link" href="" data-start="' + i + '" data-end="' + (i + show_per_page) + '">' + (total_page) + '</a>';
+			console.log('total_page:', total_page);
 			total_page++;
 		}
+
+		// After all the navigation_html are created, add the Next link 
+		// and $('#page_navigation').html(navigation_html) will create the navigation bar in the html
+		// which is located in <div id=page_navigation></div>
 		navigation_html += '<a class="next_link" href="">Next</a>';
 		navigation_html += '<a class="last_link" href="">>></a>';
 		$('#page_navigation').html(navigation_html); //.html() change all the content of elements
-		rowDisplay(0, show_per_page);
 		$('.page_link').first().addClass('active');
-	});
+		rowDisplay(0, show_per_page);
+	}
+
+	//hide() all the element and select the element you want to show
+	//slice() method selects a subset of elements based on its index	
+	function rowDisplay(startIndex, endIndex) {
+		$('#paginate tr').hide().slice(startIndex, endIndex).show();
+	}
 
 });
 
